649077e67c9ed2c88cdb2ba7437641949ecfcfca
Kevin Gorski <kevin.gorski@gmail.com>
2010-11-26 13:13:32 -0700

    Adding HTML5 input types to hotkeys plug-in

--- js/jquery.hotkeys.js
+++ js/jquery.hotkeys.js
@@ -43,8 +43,9 @@
 	
 		handleObj.handler = function( event ) {
 			// Don't fire in text-accepting inputs that we didn't directly bind to
-			if ( this !== event.target && (/textarea|select/i.test( event.target.nodeName ) ||
-				 event.target.type === "text") ) {
+			if ( this !== event.target && (/textarea|select/i.test( event.target.nodeName ) 
+				|| event.target.type === "text"
+				|| event.target.type === "number") ) {
 				return;
 			}
 			
